8d50e8699d0cfceb3877e249b4a2537b6dd2854d
chrislim2888 <chrislim2888@yahoo.com>
2014-09-03 12:53:03 +0800

    Support IPv6

--- ngx_http_ip2location_module.c
+++ ngx_http_ip2location_module.c
@@ -6,7 +6,7 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-#include "../ip2location-c-6.0.1/libIP2Location/IP2Location.h"
+#include "../ip2location-c-7.0.0/libIP2Location/IP2Location.h"
 
 /* Directive handlers */
 static char *ngx_http_ip2location_database(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
